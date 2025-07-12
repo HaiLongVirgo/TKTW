85bc17baac910dc02a757bd55ffc831ac234e7f7
HaiLongVirgo <hailongvirgo@gmail.com>
2025-07-12 20:56:38 +0700

    lab

--- PH60038/Lab1-2/LAB2/BaseExam/routes/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$action = $_GET['action'] ?? '/';
-
-match ($action) {
-    '/'         => (new HomeController)->index(),
-      'Products-show'  => (new ProductController)->show(),
-      'Products-list'  => (new ProductController)->show(),
-};
